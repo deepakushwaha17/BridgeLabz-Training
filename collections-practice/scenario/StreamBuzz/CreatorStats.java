7ab4c71f57f9e373c155796308bce36c706b273b
deepakushwaha17 <deepakushwaha155@gmail.com>
2026-02-03 11:23:44 +0530

    [Deepa Kushwaha] Add. Stream buzz scenario problem completed

---
+++ collections-practice/scenario/StreamBuzz/CreatorStats.java
@@ -0,0 +1,15 @@
+package scenario.StreamBuzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreatorStats {
+	String creatorName;
+	double[] weeklyLikes = new double[4];
+	
+	// Static list to hold all registered creators
+	public static List<CreatorStats> engagementBoard = new ArrayList<>();
+	
+	
+
+}
